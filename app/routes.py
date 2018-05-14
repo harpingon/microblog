10667a183d52d7b199fca1daf4e8dc39c2bb388d
Gary Cowell <gary.cowell@gmail.com>
2018-05-14 10:19:00 +0100

    :seedling: pagination

--- app/routes.py
+++ app/routes.py
@@ -36,7 +36,7 @@ def index():
     prev_url = url_for('index', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('index.html', title='Home', form=form,
-                           posts=posts.items)
+                           posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -140,4 +140,4 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template("index.html", title='Explore', posts=posts.items)
+    return render_template("index.html", title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
